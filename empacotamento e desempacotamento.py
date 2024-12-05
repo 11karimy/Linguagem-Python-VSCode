619d3dbd718bc7334dea23ab788e60fc40bc5242
Karimy <141510083+11karimy@users.noreply.github.com>
2024-12-05 16:50:35 -0300

    Add files via upload

---
+++ empacotamento e desempacotamento.py
@@ -0,0 +1,12 @@
+'''
+Introdução ao desempacotamento + tuples (tuplas)
+Tipo tupla - Uma lista imutável
+'''
+# _, nome, *_ = ['Maria', 'Helena', 'Luiz']
+# print(nome)
+
+nomes = ['Maria', 'Helena', 'Luiz']
+# nomes = tuple(nomes)
+# nomes = list(nomes)
+print(nomes[-1])
+print(nomes)
